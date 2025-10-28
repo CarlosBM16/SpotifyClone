05399d083659cdaafcd6a2841a58b3fe9e8892ab
Carlos Bermúdez Mancebo <carlosbermudezmancebo@gmail.com>
2025-10-28 20:00:36 +0100

    Many songs added

--- app/src/main/java/com/example/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/example/spotifyclone/MainActivity.kt
@@ -1,42 +1,28 @@
 package com.example.spotifyclone
 
 import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.spotifyclone.ui.theme.SpotifyCloneTheme
+
 class MainActivity : ComponentActivity() {
     var mediaPlayer : MediaPlayer? = null
     var songIndex by mutableIntStateOf(0)
-    val file = R.raw.iftos
     val songList = mutableListOf(
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "Imaginations from the Other Side", "Blind Guardian", file),
+        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "Imaginations From The Other Side", "Blind Guardian", R.raw.imaginations),
         SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "I'm Alive", "Blind Guardian", R.raw.imalive),
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "A Past and Future Secret", "Blind Guardian", file),
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "The Script for My Requiem", "Blind Guardian", file),
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "Terminal Exile", "Blind Guardian", file),
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "Lord of the Rings", "Blind Guardian", file),
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "Welcome to Dying", "Blind Guardian", file),
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "Somewhere Far Beyond", "Blind Guardian", file),
-        SongData(ImageData(R.drawable.igtos, "Imaginations from the Other Side"), "Imaginations from the Other Side (Reprise)", "Blind Guardian", file)
+        SongData(ImageData(R.drawable.masterofpuppets, "Master of Puppets"), "Master Of Puppets", "Metallica", R.raw.masterofpuppets),
+        SongData(ImageData(R.drawable.meliora, "Meliora"), "Square Hammer", "Ghost", R.raw.squarehammer),
+        SongData(ImageData(R.drawable.rustinpeace, "Rust in Peace"), "Hangar 18", "Megadeth", R.raw.hangar18),
+        SongData(ImageData(R.drawable.paranoid, "Paranoid"), "War Pigs", "Black Sabbath", R.raw.warpigs),
+        SongData(ImageData(R.drawable.reigninblood, "Reign in Blood"), "Angel Of Death", "Slayer", R.raw.angelofdeath),
+        SongData(ImageData(R.drawable.blizzardofozz, "Blizzard of Ozz"), "Crazy Train", "Ozzy Osbourne", R.raw.crazytrain),
+        SongData(ImageData(R.drawable.avengedsevenfold, "Avenged Sevenfold"), "A Little Piece Of Heaven", "Avenged Sevenfold", R.raw.alittlepieaceofheaven),
+        SongData(ImageData(R.drawable.somewhereintime, "Somewhere in Time"), "Wasted Years", "Iron Maiden", R.raw.wastedyears)
     )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
